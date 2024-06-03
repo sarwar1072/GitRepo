1db291cc75f5eef34eec9608cc0d32fe27734a3d
sarwar1072 <sarwar.cse.diu@gmail.com>
2024-06-03 08:14:31 +0600

    initial commit

---
+++ ProjectForGit/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectForGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Support Git by commanline");
+        }
+    }
+}
